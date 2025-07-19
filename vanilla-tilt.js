c0274b07cf5144ed5cf04cc18cc9ff9316a069f6
sunetra04 <153492251+sunetra04@users.noreply.github.com>
2025-07-19 10:17:10 +0530

    Update vanilla-tilt.js

--- vanilla-tilt.js
+++ vanilla-tilt.js
@@ -1,12 +1,7 @@
 var VanillaTilt = (function () {
 'use strict';
 
-/**
- * Created by Sergiu Șandor (micku7zu) on 1/27/2017.
- * Original idea: https://github.com/gijsroge/tilt.js
- * MIT License.
- * Version 1.8.1
- */
+
 
 class VanillaTilt {
   constructor(element, settings = {}) {
@@ -21,7 +16,7 @@ class VanillaTilt {
     this.left = null;
     this.top = null;
 
-    // for Gyroscope sampling
+
     this.gammazero = null;
     this.betazero = null;
     this.lastgammazero = null;
@@ -68,10 +63,7 @@ class VanillaTilt {
     return setting === "" || setting === true || setting === 1;
   }
 
-  /**
-   * Method returns element what will be listen mouse events
-   * @return {Node}
-   */
+ 
   getElementListener() {
     if (this.fullPageListening) {
       return window.document;
@@ -92,10 +84,7 @@ class VanillaTilt {
     return this.element;
   }
 
-  /**
-   * Method set listen methods for this.elementListener
-   * @return {Node}
-   */
+
   addEventListeners() {
     this.onMouseEnterBind = this.onMouseEnter.bind(this);
     this.onMouseMoveBind = this.onMouseMove.bind(this);
@@ -116,9 +105,7 @@ class VanillaTilt {
     }
   }
 
-  /**
-   * Method remove event listeners from current this.elementListener
-   */
+
   removeEventListeners() {
     this.elementListener.removeEventListener("mouseenter", this.onMouseEnterBind);
     this.elementListener.removeEventListener("mouseleave", this.onMouseLeaveBind);
@@ -305,14 +292,9 @@ class VanillaTilt {
     this.updateCall = null;
   }
 
-  /**
-   * Appends the glare element (if glarePrerender equals false)
-   * and sets the default style
-   */
+
   prepareGlare() {
-    // If option pre-render is enabled we assume all html/css is present for an optimal glare effect.
     if (!this.glarePrerender) {
-      // Create glare element
       const jsTiltGlare = document.createElement("div");
       jsTiltGlare.classList.add("js-tilt-glare");
 
@@ -395,29 +377,7 @@ class VanillaTilt {
 
   }
 
-  /**
-   * Method return patched settings of instance
-   * @param {boolean} settings.reverse - reverse the tilt direction
-   * @param {number} settings.max - max tilt rotation (degrees)
-   * @param {startX} settings.startX - the starting tilt on the X axis, in degrees. Default: 0
-   * @param {startY} settings.startY - the starting tilt on the Y axis, in degrees. Default: 0
-   * @param {number} settings.perspective - Transform perspective, the lower the more extreme the tilt gets
-   * @param {string} settings.easing - Easing on enter/exit
-   * @param {number} settings.scale - 2 = 200%, 1.5 = 150%, etc..
-   * @param {number} settings.speed - Speed of the enter/exit transition
-   * @param {boolean} settings.transition - Set a transition on enter/exit
-   * @param {string|null} settings.axis - What axis should be enabled. Can be "x" or "y"
-   * @param {boolean} settings.glare - if it should have a "glare" effect
-   * @param {number} settings.max-glare - the maximum "glare" opacity (1 = 100%, 0.5 = 50%)
-   * @param {boolean} settings.glare-prerender - false = VanillaTilt creates the glare elements for you, otherwise
-   * @param {boolean} settings.full-page-listening - If true, parallax effect will listen to mouse move events on the whole document, not only the selected element
-   * @param {string|object} settings.mouse-event-element - String selector or link to HTML-element what will be listen mouse events
-   * @param {boolean} settings.reset - false = If the tilt effect has to be reset on exit
-   * @param {boolean} settings.reset-to-start - true = On reset event (mouse leave) will return to initial start angle (if startX or startY is set)
-   * @param {gyroscope} settings.gyroscope - Enable tilting by deviceorientation events
-   * @param {gyroscopeSensitivity} settings.gyroscopeSensitivity - Between 0 and 1 - The angle at which max tilt position is reached. 1 = 90deg, 0.5 = 45deg, etc..
-   * @param {gyroscopeSamples} settings.gyroscopeSamples - How many gyroscope moves to decide the starting position.
-   */
+  
   extendSettings(settings) {
     let defaultSettings = {
       reverse: false,
@@ -487,12 +447,10 @@ class VanillaTilt {
 }
 
 if (typeof document !== "undefined") {
-  /* expose the class to window */
+
   window.VanillaTilt = VanillaTilt;
 
-  /**
-   * Auto load
-   */
+
   VanillaTilt.init(document.querySelectorAll("[data-tilt]"));
 }
 
